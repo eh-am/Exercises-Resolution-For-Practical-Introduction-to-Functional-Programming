cdd883ce881daff90916d5b473560b69250ee590
eduardo aleixo <eduardoaleixomartins@gmail.com>
2015-01-26 12:20:33 -0200

    finish ex1

---
+++ ex1.py
@@ -0,0 +1,15 @@
+#Non-functional:
+
+# names = ['Mary', 'Isla', 'Sam']
+
+# for i in range(len(names)):
+#     names[i] = hash(names[i])
+
+# print names
+# => [6306819796133686941, 8135353348168144921, -1228887169324443034]
+
+names = ['Mary', 'Isla', 'Sam']
+
+hashed_names = map(lambda x: hash(x), names)
+
+print hashed_names
